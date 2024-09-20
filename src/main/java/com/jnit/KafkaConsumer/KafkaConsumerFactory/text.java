6d53800f7f27472fa8053f5b005ae078bda943f2
lokesh465 <lokesh.465@gmail.com>
2024-09-20 11:09:03 -0500

    Update text.java

--- src/main/java/com/jnit/KafkaConsumer/KafkaConsumerFactory/text.java
+++ src/main/java/com/jnit/KafkaConsumer/KafkaConsumerFactory/text.java
@@ -1,138 +1,40 @@
-@Value("${mongoDB.collection}")
-private String mongoCollection;
+public Mono<Object> fluxParser(String kafkaInContractPayload, String contractIdentifier) {
+    Logger.info("Started comparing Kafka record with Mongo record for Contract Identifier: {}", contractIdentifier);
 
-@Value("${mongoDB.field}")
-private String mongoField;
+    // Create the query
+    var query = new Query();
+    query.addCriteria(Criteria.where(PayloadConstants.Contract_Identifier).is(contractIdentifier));
 
-@Value("${mongoDB.database}")
-private String database;
+    // Fetch document from MongoDB
+    return Mono.justOrEmpty(mongoTemplate.findOne(query, Document.class, mongoCollection))
+            .flatMap(doc -> {
+                Logger.info("Retrieved Document from Mongo based on Contract Identifier: {}", contractIdentifier);
+                
+                var mongoContractPayload = new JSONObject(doc.toJson());
 
-@Value("${optimized.payload.attributes}")
-private String[] attributes;
+                // Check for contract type
+                if (mongoContractPayload.has(PayloadConstants.CASH_CONTRACT)) {
+                    mongoContractPayload = mongoContractPayload.getJSONObject(PayloadConstants.CASH_CONTRACT);
+                } else if (mongoContractPayload.has(PayloadConstants.SWAP_CONTRACT)) {
+                    mongoContractPayload = mongoContractPayload.getJSONObject(PayloadConstants.SWAP_CONTRACT);
+                }
 
-@Value("${optimized.payload.attributes.array}")
-private String[] attributesArray;
+                var objectMapper = new ObjectMapper();
+                var mongoContractPayloadStr = mongoContractPayload.toString();
 
-public void createOptimizedPayload(String contractPayloadReceived) throws Exception {
+                // Parse and merge Kafka and Mongo payloads
+                return Mono.fromCallable(() -> {
+                    var mongoMap = objectMapper.readValue(mongoContractPayloadStr, Map.class);
+                    var kafkaMap = objectMapper.readValue(kafkaInContractPayload, Map.class);
 
-    LOGGER.info("Started creating Optimized Payload");
-    JSONObject contractPayload = new JSONObject(contractPayloadReceived);
+                    // Merge keys from Kafka into Mongo
+                    kafkaMap.forEach(mongoMap::putIfAbsent);
 
-    String contractIdentifier = String.valueOf(extractValuesOutOfPayload(contractPayload, "contractIdentifier").orElse(""));
-
-    MdcInfo mdcInfo = new MdcInfo();
-
-    LOGGER.info("Started parsing into JSONObject to create optimized Payload");
-    JSONObject optimizedJSON = new JSONObject();
-    optimizedJSON.put("lastupdatedDateTime", new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss:SSSZ").format(new Date()));
-
-    processContract(contractPayload, PayloadConstants.SWAP_CONTRACT, optimizedJSON, mdcInfo, contractIdentifier);
-    processContract(contractPayload, PayloadConstants.CASH_CONTRACT, optimizedJSON, mdcInfo, contractIdentifier);
-    processContract(contractPayload, PayloadConstants.LIVE_TRADE_CONTRACT, optimizedJSON, mdcInfo, contractIdentifier);
-
-    addAttributesToOptimizedPayload(contractPayload, optimizedJSON, attributes);
-    addAttributesArrayToOptimizedPayload(contractPayload, optimizedJSON, attributesArray);
-
-    LOGGER.info("################# Optimized JSON: " + optimizedJSON);
-
-    persistData(optimizedJSON);
-    sendDataToS3(optimizedJSON);
-
-    LOGGER.info("Completed creating Optimized payload and sent to Mongo, S3 and Kafka out topic");
-}
-
-private void processContract(JSONObject contractPayload, String contractType, JSONObject optimizedJSON, MdcInfo mdcInfo, String contractIdentifier) {
-    extractValuesOutOfPayload(contractPayload, contractType).ifPresent(contract -> {
-        if (contract instanceof JSONArray) {
-            JSONArray jsonArray = (JSONArray) contract;
-            if (jsonArray.length() > 0 && jsonArray.get(0) instanceof JSONObject) {
-                contract = jsonArray.getJSONObject(0);
-                mdcInfo.setEventType(contractType);
-                mdcInfo.setContractIdentifier(contractIdentifier);
-            }
-        }
-        optimizedJSON.put(contractType, contract);
-    });
-}
-
-private void addAttributesToOptimizedPayload(JSONObject contractPayload, JSONObject optimizedJSON, String[] attributes) {
-    for (String elementKey : attributes) {
-        Object elementValue = extractValuesOutOfPayload(contractPayload, elementKey).orElse(JSONObject.NULL);
-        optimizedJSON.put(elementKey, elementValue);
-    }
-}
-
-private void addAttributesArrayToOptimizedPayload(JSONObject contractPayload, JSONObject optimizedJSON, String[] attributesArray) {
-    for (String elementKey : attributesArray) {
-        Set<Object> elementValueSet = extractValuesArrayOutOfPayload(contractPayload, elementKey);
-        JSONArray elementValueArray = new JSONArray(elementValueSet);
-        optimizedJSON.put(elementKey, elementValueArray.isEmpty() ? JSONObject.NULL : elementValueArray);
-    }
-}
-
-private void persistData(JSONObject optimizedJSON) throws JsonPersistenceException {
-    try {
-        persisJsonToMongoDB.processData(optimizedJSON.toString(), mongoCollection, mongoField);
-        LOGGER.info("----Contract data is persisted into DB");
-    } catch (Exception e) {
-        LOGGER.error("Exception while persisting JSON: " + e.getMessage());
-        throw new JsonPersistenceException("Exception while persisting JSON", e);
-    }
-}
-
-private void sendDataToS3(JSONObject optimizedJSON) throws JsonPersistenceException {
-    try {
-        LOGGER.info("-----------> Started persisting JSON to S3");
-        deltaService.insertRecordsWithDate(optimizedJSON.toString(), deltaPath);
-        LOGGER.info("------------> Contract data is persisted into S3");
-    } catch (Exception e) {
-        LOGGER.error("Exception while sending JSON to S3: " + e.getMessage());
-        throw new JsonPersistenceException("Exception while sending JSON to S3", e);
-    }
-}
-
-private Optional<Object> extractValuesOutOfPayload(JSONObject contractPayload, String elementKey) {
-    if (contractPayload.has(elementKey)) {
-        return Optional.of(contractPayload.get(elementKey));
-    }
-    
-    return contractPayload.keySet().stream()
-        .map(k -> contractPayload.get(k))
-        .flatMap(value -> {
-            if (value instanceof JSONObject) {
-                return extractValuesOutOfPayload((JSONObject) value, elementKey).stream();
-            } else if (value instanceof JSONArray) {
-                return IntStream.range(0, ((JSONArray) value).length())
-                    .mapToObj(((JSONArray) value)::get)
-                    .filter(JSONObject.class::isInstance)
-                    .map(JSONObject.class::cast)
-                    .map(nestedObject -> extractValuesOutOfPayload(nestedObject, elementKey))
-                    .flatMap(Optional::stream);
-            }
-            return Stream.empty();
-        })
-        .findFirst();
-}
-
-private Set<Object> extractValuesArrayOutOfPayload(Object sourceJson, String targetKey) {
-    Set<Object> resultSet = new HashSet<>();
-
-    if (sourceJson instanceof JSONObject) {
-        JSONObject jsonObject = (JSONObject) sourceJson;
-        jsonObject.keySet().forEach(key -> {
-            Object value = jsonObject.opt(key);
-            if (key.equals(targetKey) && (value instanceof String || value instanceof Integer)) {
-                resultSet.add(value);
-            } else {
-                resultSet.addAll(extractValuesArrayOutOfPayload(value, targetKey));
-            }
-        });
-    } else if (sourceJson instanceof JSONArray) {
-        JSONArray jsonArray = (JSONArray) sourceJson;
-        IntStream.range(0, jsonArray.length())
-            .mapToObj(jsonArray::get)
-            .forEach(element -> resultSet.addAll(extractValuesArrayOutOfPayload(element, targetKey)));
-    }
-
-    return resultSet;
+                    return mongoMap;
+                });
+            })
+            .switchIfEmpty(Mono.defer(() -> {
+                Logger.warn("No document found for Contract Identifier: {}", contractIdentifier);
+                return Mono.just(kafkaInContractPayload);
+            }));
 }
