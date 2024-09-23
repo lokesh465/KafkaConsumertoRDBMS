b40aeff6da6b9b549b00e62c8c133a0405419d4c
lokesh465 <lokesh.465@gmail.com>
2024-09-23 11:09:09 -0500

    Update text.java

--- src/main/java/com/jnit/KafkaConsumer/KafkaConsumerFactory/text.java
+++ src/main/java/com/jnit/KafkaConsumer/KafkaConsumerFactory/text.java
@@ -1,4 +1,3 @@
-
 import java.util.concurrent.CompletableFuture;
 import org.springframework.kafka.support.SendResult;
 import org.springframework.util.concurrent.ListenableFuture;
@@ -8,23 +7,27 @@ public CompletableFuture<String> kafkaWrite(String inputJson, String contractPro
     KafkaRegister kafkaRegister = new KafkaRegister(applicationContext);
     KafkaTemplate<String, String> template = kafkaRegister.getProducerKafkaTemplate(contractProducerType);
 
-    // Sending the message to Kafka
-    ListenableFuture<SendResult<String, String>> future = template.send(kafkaRegister.getProducerTopic(contractProducerType), UUID.randomUUID().toString(), inputJson);
+    // Sending the message to Kafka and obtaining ListenableFuture
+    ListenableFuture<SendResult<String, String>> listenableFuture = template.send(kafkaRegister.getProducerTopic(contractProducerType), UUID.randomUUID().toString(), inputJson);
 
-    // Converting ListenableFuture to CompletableFuture
+    // Create a CompletableFuture
     CompletableFuture<String> completableFuture = new CompletableFuture<>();
 
-    future.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
+    // Add a callback to the ListenableFuture
+    listenableFuture.addCallback(new ListenableFutureCallback<SendResult<String, String>>() {
         @Override
         public void onSuccess(SendResult<String, String> result) {
+            // Completing the CompletableFuture with success
             completableFuture.complete("Success");
         }
 
         @Override
         public void onFailure(Throwable ex) {
+            // Completing the CompletableFuture exceptionally in case of failure
             completableFuture.completeExceptionally(ex);
         }
     });
 
+    // Return the CompletableFuture to be handled asynchronously
     return completableFuture;
 }
